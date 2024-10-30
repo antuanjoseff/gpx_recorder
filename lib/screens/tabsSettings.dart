9ae16b9b8d3afd245b91f9daad4400914c551f21
unknown <tonidelacalle@gmail.com>
2024-10-30 11:58:49 +0100

    wip

---
+++ lib/screens/tabsSettings.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class TabSettings extends StatefulWidget {
+  const TabSettings({super.key});
+
+  @override
+  State<TabSettings> createState() => _TabSettingsState();
+}
+
+class _TabSettingsState extends State<TabSettings> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+          appBar: AppBar(
+            bottom: const TabBar(
+              tabs: [
+                Tab(icon: Icon(Icons.directions_car)),
+                Tab(icon: Icon(Icons.directions_transit)),
+                Tab(icon: Icon(Icons.directions_bike)),
+              ],
+            ),
+          ),
+          body: const TabBarView(
+            children: [
+              Icon(Icons.directions_car),
+              Icon(Icons.directions_transit),
+              Icon(Icons.directions_bike),
+            ],
+          )),
+    );
+  }
+}
